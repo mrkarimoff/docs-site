96e84cb9a22ec9eb5036be882425ec641c406f9a
mrkarimoff <mrkarimoff99@gmail.com>
2024-03-04 16:33:22 +0500

    trying verceljson conf

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function RootPage() {
-  redirect('/'); // temporarily redirecting to the root page
+  return redirect('/');
 }
--- next.config.js
+++ next.config.js
@@ -10,22 +10,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  redirects: async () => {
-    return [
-      {
-        source: '/(en|ru)/:path*',
-        destination: '/:path*',
-        permanent: true,
-        locale: false,
-      },
-      {
-        source: '/:path*',
-        destination: '/en/:path*',
-        permanent: true,
-        locale: false,
-      },
-    ];
-  },
 };
 
 // Merge NextIntl config with Next.js config
