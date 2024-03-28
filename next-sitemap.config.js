b48e93029dbb5d1643e106ab42d59411227e1c03
mrkarimoff <mrkarimoff99@gmail.com>
2024-03-28 14:48:03 +0500

    Update siteUrl in next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://documentation.networkcanvas.dev',
+  siteUrl: process.env.SITE_URL || 'https://docs-site-beryl.vercel.app',
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
